456e88ad681b0c16e77dca887aafc68b3dfed7a6
Halalisan Alex <halalisanalexandru@yahoo.com>
2026-09-23 15:00:14 +0300

    add home and not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepositComponent } from './components/deposit/deposit.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HelloComponent } from './components/hello/hello.component';
 
 const routes: Routes = [
+  { path: '', component: HelloComponent },
   { path: 'deposit', component: DepositComponent },
   { path: 'recipes', component: RecipesComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
